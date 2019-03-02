e6e38f7d8a1c43ad4b08ef78c2f6b92ada482ed8
btjanaka <bryon.tjanaka@gmail.com>
2019-03-01 21:58:56 -0800

    Add Hackerrank Kaprekar Numbers

---
+++ hackerrank/modified-kaprekar-numbers.cpp
@@ -0,0 +1,43 @@
+// Author: btjanaka (Bryon Tjanaka)
+// Problem: (Hackerrank) modified-kaprekar-numbers
+#include <bits/stdc++.h>
+#define GET(x) scanf("%d", &x)
+#define GED(x) scanf("%lf", &x)
+typedef long long ll;
+using namespace std;
+typedef pair<int, int> ii;
+
+bool kaprekar(ll i) {
+  ll sq = i * i;
+  ll left = sq;
+  ll right = 0;
+  ll place = 1;
+  int d = 0;
+  for (sq; sq > 0; ++d, sq /= 10)
+    ;
+  for (int j = 0; j < (d / 2) + (d % 2); ++j) {
+    right = right + (left % 10) * place;
+    place *= 10;
+    left /= 10;
+  }
+  return left + right == i;
+}
+
+int main() {
+  ll p, q;
+  scanf("%lld %lld", &p, &q);
+  bool first = true;
+  for (ll i = p; i <= q; ++i) {
+    if (kaprekar(i)) {
+      if (first) {
+        first = false;
+      } else {
+        putchar(' ');
+      }
+      printf("%lld", i);
+    }
+  }
+  if (first) printf("INVALID RANGE");
+  putchar('\n');
+  return 0;
+}
