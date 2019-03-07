593466e3d57748cb6a9ef2efb0733dfede6fe08f
btjanaka <bryon.tjanaka@gmail.com>
2019-03-07 12:23:57 -0800

    Add Hackerrank BFS

---
+++ hackerrank/breadth-first-search-shortest-reach.cpp
@@ -0,0 +1,63 @@
+// Author: btjanaka (Bryon Tjanaka)
+// Problem: (Hackerrank) breadth-first-search-shortest-reach
+#include <bits/stdc++.h>
+#define GET(x) scanf("%d", &x)
+#define GED(x) scanf("%lf", &x)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main() {
+  int ca;
+  GET(ca);
+  while (ca--) {
+    int n, m;
+    GET(n);
+    GET(m);
+    vector<vector<int>> g(n);
+    for (int i = 0; i < m; ++i) {
+      int u, v;
+      GET(u);
+      GET(v);
+      --u;
+      --v;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+
+    int s;
+    GET(s);
+    --s;
+
+    vector<bool> visited(n, false);
+    vector<int> dist(n, -1);
+    queue<ii> q;
+    q.push({s, 0});
+    while (!q.empty()) {
+      ii cur = q.front();
+      q.pop();
+      int u = cur.first, d = cur.second;
+      dist[u] = d * 6;
+      for (int v : g[u]) {
+        if (!visited[v]) {
+          q.push({v, d + 1});
+          visited[v] = true;
+        }
+      }
+    }
+
+    bool first = true;
+    for (int i = 0; i < n; ++i) {
+      if (i == s) continue;
+      if (first) {
+        first = false;
+      } else {
+        printf(" ");
+      }
+      printf("%d", dist[i]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
