f2ab0b01d7ef53121806af739b013fbac8c5a802
btjanaka <bryon.tjanaka@gmail.com>
2019-04-03 15:59:05 -0700

    Add codeforces/952a.cpp

---
+++ codeforces/952a.cpp
@@ -0,0 +1,16 @@
+// Author: btjanaka (Bryon Tjanaka)
+// Problem: (CodeForces) 952a
+#include <bits/stdc++.h>
+#define GET(x) scanf("%d", &x)
+#define GED(x) scanf("%lf", &x)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main() {
+  int x;
+  GET(x);
+  printf("%d\n", x % 2);
+  return 0;
+}
