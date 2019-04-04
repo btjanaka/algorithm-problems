8d945642f7dc5b8f56aa52f1e820437c6a8fc54a
btjanaka <bryon.tjanaka@gmail.com>
2019-04-03 23:58:05 -0700

    Add leetcode/66.java

---
+++ leetcode/66.java
@@ -0,0 +1,26 @@
+// Author: btjanaka (Bryon Tjanaka)
+// Problem: (LeetCode) 66
+class Solution {
+  public int[] plusOne(int[] digits) {
+    boolean extra = false;
+    for (int i = digits.length - 1; i >= 0; --i) {
+      if (digits[i] == 9) {
+        digits[i] = 0;
+        if (i == 0)
+          extra = true;
+      } else {
+        ++digits[i];
+        break;
+      }
+    }
+    if (extra) {
+      int[] res = new int[digits.length + 1];
+      res[0] = 1;
+      for (int i = 1; i < res.length; ++i) {
+        res[i] = digits[i - 1];
+      }
+      return res;
+    }
+    return digits;
+  }
+}
