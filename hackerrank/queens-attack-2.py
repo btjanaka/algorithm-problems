3ae6008ea0aae83d10c0bd262889b050c7b7603d
btjanaka <bryon.tjanaka@gmail.com>
2019-04-14 21:40:30 -0700

    Add hackerrank/queens-attack-2.py

---
+++ hackerrank/queens-attack-2.py
@@ -0,0 +1,29 @@
+# Author: btjanaka (Bryon Tjanaka)
+# Problem: (HackerRank) queens-attack-2
+import sys
+from collections import defaultdict
+
+n, k = map(int, input().strip().split())
+qr, qc = map(int, input().strip().split())
+
+# store occupied columns on each row
+obstacles = defaultdict(set)
+
+for _ in range(k):
+    r, c = map(int, input().strip().split())
+    obstacles[r].add(c)
+
+count = 0
+dr = [1, 1, 1, 0, 0, -1, -1, -1]
+dc = [1, 0, -1, 1, -1, 1, 0, -1]
+for d in range(8):
+    cur = [qr, qc]
+    while True:
+        cur[0] += dr[d]
+        cur[1] += dc[d]
+        if cur[0] > n or cur[0] <= 0 or cur[1] > n or cur[1] <= 0 or \
+            (cur[0] in obstacles and cur[1] in obstacles[cur[0]]):
+            break
+        count += 1
+
+print(count)
