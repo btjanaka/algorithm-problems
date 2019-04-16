d23c13175147567de150427d68235e952be62ee1
btjanaka <bryon.tjanaka@gmail.com>
2019-04-16 01:34:42 -0700

    Add quick-select to LeetCode 215

--- leetcode/215.cpp
+++ leetcode/215.cpp
@@ -1,5 +1,39 @@
 // Author: btjanaka (Bryon Tjanaka)
 // Problem: (Leetcode) 215
+
+// Solution 1: Quick-select - O(n)
+class Solution {
+ public:
+  // Implementation of quick-select with O(n) extra memory (for simplicity)
+  // L and R are inclusive
+  int findKthLargest(vector<int>& nums, vector<int>& buf, int k, int L, int R) {
+    if (L == R) return nums[L];
+    int pivot = nums[L + rand() % (R - L + 1)];
+
+    int L_i = L, R_i = R;
+    for (int i = L; i <= R; ++i) {
+      if (nums[i] > pivot) {
+        buf[L_i++] = nums[i];
+      } else if (nums[i] < pivot) {
+        buf[R_i--] = nums[i];
+      }
+    }
+
+    for (int i = L; i <= R; ++i) nums[i] = buf[i];
+
+    if (k < L_i) return findKthLargest(nums, buf, k, L, L_i - 1);
+    if (k > R_i) return findKthLargest(nums, buf, k, R_i + 1, R);
+    return pivot;
+  }
+
+  int findKthLargest(vector<int>& nums, int k) {
+    srand(time(NULL));
+    vector<int> buf(nums.size());
+    return findKthLargest(nums, buf, k - 1, 0, nums.size() - 1);
+  }
+};
+
+// Solution 2: Priority Queue - O(n log n)
 class Solution {
  public:
   int findKthLargest(vector<int>& nums, int k) {
