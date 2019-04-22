e23304d1ab821955d8670c10c8c24b028a3f7296
btjanaka <bryon.tjanaka@gmail.com>
2019-04-21 23:49:29 -0700

    Add leetcode/79.cpp

---
+++ leetcode/79.cpp
@@ -0,0 +1,40 @@
+// Author: btjanaka (Bryon Tjanaka)
+// Problem: (LeetCode) 79
+class Solution {
+ public:
+  int dr[4] = {-1, 0, 1, 0};
+  int dc[4] = {0, -1, 0, 1};
+
+  bool dfs(int r, int c, int i, const vector<vector<char>>& board,
+           const string& word, vector<vector<bool>>& visited) {
+    if (i == word.size()) return true;
+    if (!(r < board.size() && r >= 0 && c < board[0].size() && c >= 0) ||
+        visited[r][c])
+      return false;
+    visited[r][c] = true;
+
+    bool ok = false;
+    if (word[i] == board[r][c]) {
+      for (int j = 0; j < 4; ++j) {
+        if (!ok) ok |= dfs(dr[j] + r, dc[j] + c, i + 1, board, word, visited);
+      }
+    }
+    visited[r][c] = false;
+    return ok;
+  }
+
+  bool exist(vector<vector<char>>& board, const string& word) {
+    int rows = board.size();
+    int cols = board[0].size();
+    if (word.size() > rows * cols) return false;
+    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+    for (int r = 0; r < rows; ++r) {
+      for (int c = 0; c < cols; ++c) {
+        if (board[r][c] == word[0]) {
+          if (dfs(r, c, 0, board, word, visited)) return true;
+        }
+      }
+    }
+    return false;
+  }
+};
