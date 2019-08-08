5b6dcdfd77a93225fa2beac055a44637e5f26e12
btjanaka <bryon.tjanaka@gmail.com>
2019-08-08 08:39:42 -0700

    Add HackerRank string-mingling.hs

---
+++ hackerrank/string-mingling.hs
@@ -0,0 +1,13 @@
+-- Author: btjanaka (Bryon Tjanaka)
+-- Problem: (HackerRank) string-mingling
+-- Title: String Mingling
+-- Link: https://www.hackerrank.com/challenges/string-mingling/problem
+-- Idea: (implementation)
+-- Difficulty: easy
+-- Tags: fp
+import Data.List
+
+main = do
+  p <- getLine
+  q <- getLine
+  putStrLn $ concat $ transpose [p, q]
