4212a2d6138a26a990202c272f0470c146fb71cd
btjanaka <bryon.tjanaka@gmail.com>
2019-08-10 15:52:53 -0700

    Add hackerrank/string-reduction.hs

---
+++ hackerrank/string-reduction.hs
@@ -0,0 +1,19 @@
+-- Author: btjanaka (Bryon Tjanaka)
+-- Problem: (HackerRank) string-reduction
+-- Title: String Reduction
+-- Link: https://www.hackerrank.com/challenges/string-reductions/problem
+-- Idea: (implementation)
+-- Difficulty: easy
+-- Tags: fp
+import qualified Data.Set as Set
+
+reduceString :: [Char] -> Set.Set Char -> [Char]
+reduceString [] _ = []
+reduceString (x:xs) cur =
+  if Set.member x cur
+    then reduceString xs cur
+    else x : reduceString xs (Set.insert x cur)
+
+main = do
+  s <- getLine
+  putStrLn $ reduceString s Set.empty
