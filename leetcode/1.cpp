62adfbb9119990120f7bce7475e2581f07b65e57
btjanaka <bryon.tjanaka@gmail.com>
2019-09-24 23:56:20 -0700

    Add comments to leetcode 1.cpp

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,5 +1,15 @@
 // Author: btjanaka (Bryon Tjanaka)
 // Problem: (Leetcode) 1
+// Title: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Idea: The easy solution is to check every pair of elements, but this is
+// O(n^2). To achieve O(n), the idea is to use a hash set that keeps track of
+// which elements we have seen so far while iterating through the list. Then,
+// for the value x, we can check if we have target - x in the set. In this
+// solution, we specifically use a hash map instead of a set because we need to
+// store the indices.
+// Difficulty: easy
+// Tags: set
 class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
@@ -11,8 +21,8 @@ class Solution {
       }
       numtoindex[target - curr] = i;
     }
-    // Technically not necessary, since problem specifies there will be a
-    // solution
+    // Technically not necessary, as the problem specifies we will always have
+    // exactly one solution.
     return {-1, -1};
   }
 };
