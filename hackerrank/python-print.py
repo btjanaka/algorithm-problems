e04a38a9e8b6b1693596a559f72566af80331003
btjanaka <bryon.tjanaka@gmail.com>
2019-10-08 00:01:14 -0700

    Add hackerrank/python-print.py

---
+++ hackerrank/python-print.py
@@ -0,0 +1,14 @@
+# Author: btjanaka (Bryon Tjanaka)
+# Problem: (HackerRank) python-print
+# Title: Python Print
+# Link: https://www.hackerrank.com/challenges/python-print/problem
+# Idea: Print the numbers.
+# Difficulty: easy
+# Tags: python
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in xrange(1, n + 1):
+        print(i, end="")
+    print()
