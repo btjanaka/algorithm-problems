76b063f0af1ba148320ef1261db8cd3d7a78d5c6
btjanaka <bryon.tjanaka@gmail.com>
2020-03-29 22:37:51 -0700

    Update metadata and filenames in CodeForces

--- codeforces/546a.py
+++ codeforces/546a.py
@@ -1,1 +1,8 @@
+# Author: btjanaka (Bryon Tjanaka)
+# Problem: (CodeForces) 546a
+# Title: Soldier and Bananas
+# Link: https://codeforces.com/problemset/problem/546/A
+# Idea: See if you can figure out the code golf :D
+# Difficulty: easy
+# Tags: implementation
 k,n,w=map(int,input().split());print(max(0,w*(w+1)//2*k-n))
