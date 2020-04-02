0e1809008d0d34919392c0c44caf564089e65907
btjanaka <bryon@btjanaka.net>
2020-04-02 14:46:28 -0700

    Add codeforces/1331a.py

---
+++ codeforces/1331a.py
@@ -0,0 +1,8 @@
+# Author: btjanaka (Bryon Tjanaka)
+# Problem: (CodeForces) 1331a
+# Title: Is it rated?
+# Link: https://codeforces.com/contest/1331/problem/A
+# Idea: Part of April Fool's contets 2020.
+# Difficulty: easy
+# Tags: april-fools
+print("no")
